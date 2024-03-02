f6a1a5456ae14b183fb328baa97d4ad9b8e36790
AlCher38 <154320556+AlCher38@users.noreply.github.com>
2024-03-02 13:11:58 +0300

    Update item.py

--- src/item.py
+++ src/item.py
@@ -1,9 +1,19 @@
+import os
+from csv import DictReader
+
+
+class InstantiateCSVError(Exception):
+    pass
+
+
 class Item:
     """
     Класс для представления товара в магазине.
     """
     pay_rate = 1.0
     all = []
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    dir_path = dir_path + '/items.csv'
 
     def __init__(self, name: str, price: float, quantity: int) -> None:
         """
@@ -13,7 +23,76 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        pass
+        super().__init__()
+        self.__name = name
+        self.price = price
+        self.quantity = quantity
+        self.all.append(self)
+
+    def __repr__(self) -> str:
+        """Магический метод возвращающий информацию для разработчика"""
+        return f"{self.__class__.__name__}('{self.__name}', {self.price}, {self.quantity})"
+
+    def __str__(self) -> str:
+        """Магический метод возвращающий информацию для пользователя"""
+        return f"{self.__name}"
+
+    def __add__(self, other):
+        """
+        Метод для сложения количества товара в магазине
+        """
+        if isinstance(other, Item):
+            return self.quantity + other.quantity
+        raise Exception("Неверный формат сложения")
+
+    @classmethod
+    def instantiate_from_csv(cls, default=False) -> None:
+        """
+        Создает экземпляры класса на основе таблицы хранящейся в items.csv
+        """
+        try:
+            file_csv = default if default else cls.dir_path
+            with open(file_csv, encoding='utf-8') as csv_file:
+                csv_reader = DictReader(csv_file)
+                cls.all = []
+                for row in csv_reader:
+                    try:
+                        name = row['name']
+                        price = row['price']
+                        quantity = row['quantity']
+                        cls(name, float(price), int(quantity))
+                    except KeyError:
+                        raise InstantiateCSVError('Файл item.csv поврежден')
+        except FileNotFoundError:
+            raise FileNotFoundError("Отсутствует файл item.csv")
+
+    @staticmethod
+    def string_to_number(number: str) -> int:
+        """
+        Преобразовывает строки-числа в числа
+        """
+        if "." in number:
+            return int(float(number))
+        else:
+            return int(number)
+
+    @property
+    def name(self) -> str:
+        """
+        Геттер, который возвращает наименование товара
+        """
+        return self.__name
+
+    @name.setter
+    def name(self, name: str) -> None:
+        """
+        Сеттер, который проверяет длину названия товара,
+        и если она не больше 10 символов то записывает ее
+        """
+        if len(name) <= 10:
+            self.__name = name
+        else:
+            raise Exception('Длина товара превышает 10 символов')
 
     def calculate_total_price(self) -> float:
         """
@@ -21,10 +100,10 @@ class Item:
 
         :return: Общая стоимость товара.
         """
-        pass
+        return self.price * self.quantity
 
     def apply_discount(self) -> None:
         """
         Применяет установленную скидку для конкретного товара.
         """
-        pass
+        self.price *= self.pay_rate
