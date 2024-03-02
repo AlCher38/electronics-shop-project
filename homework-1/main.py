2ec4d691d28e02cae91644ce7b492bc17c13b8d5
AlCher38 <154320556+AlCher38@users.noreply.github.com>
2024-03-02 13:08:57 +0300

    Update main.py

--- homework-1/main.py
+++ homework-1/main.py
@@ -1,18 +1,19 @@
-from src.item import Item
+from src.stack import Node, Stack
 
 if __name__ == '__main__':
-    item1 = Item("Смартфон", 10000, 20)
-    item2 = Item("Ноутбук", 20000, 5)
+    n1 = Node(5, None)
+    n2 = Node('a', n1)
+    print(n1.data)  # 5
+    print(n2.data)  # a
+    print(n1)  # <__main__.Node object at 0x0000022803036050>
+    print(n2.next_node)  # <__main__.Node object at 0x0000022803036050>
 
-    print(item1.calculate_total_price())  # 200000
-    print(item2.calculate_total_price())  # 100000
-
-    # устанавливаем новый уровень цен
-    Item.pay_rate = 0.8
-    # применяем скидку
-    item1.apply_discount()
-
-    print(item1.price)  # 8000.0
-    print(item2.price)  # 20000
-
-    print(Item.all)  # [<__main__.Item object at 0x000001EC6250C690>, <__main__.Item object at 0x000001EC6250C6D0>]
+    stack = Stack()
+    stack.push('data1')
+    stack.push('data2')
+    stack.push('data3')
+    print(stack.top.data)  # data3
+    print(stack.top.next_node.data)  # data2
+    print(stack.top.next_node.next_node.data)  # data1
+    print(stack.top.next_node.next_node.next_node)  # None
+    print(stack.top.next_node.next_node.next_node.data)  # AttributeError: 'NoneType' object has no attribute 'data'
